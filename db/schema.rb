0fb6b7819d4258830a92d2123f59b0f8c75dab81
smrity-ku-nascenia <resheta@bdipo.com>
2017-12-22 10:13:25 +0600

    syntax is corrected according to rubocop coding convention tool

--- db/schema.rb
+++ db/schema.rb
@@ -46,5 +46,4 @@ ActiveRecord::Schema.define(version: 20171220050719) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ password = '123456'
 
 1.upto(5) do |i|
   User.create(
-      email: "user_#{i}@bdipo.com",
-      password: password,
-      password_confirmation: password
+    email: "user_#{i}@bdipo.com",
+    password: password,
+    password_confirmation: password
   )
 end
